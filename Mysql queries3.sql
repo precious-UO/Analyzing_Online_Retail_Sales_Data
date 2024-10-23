f8549df07a0d439aac68b997d6eb91d2a13120a9
precious_UO <4preciousokekeani@gmail.com>
2024-10-23 10:45:57 +0100

    Update Queries

--- Mysql queries3.sql
+++ Mysql queries3.sql
@@ -54,5 +54,37 @@ ALTER TABLE onlineretail MODIFY Quantity INT;
 ALTER TABLE onlineretail MODIFY InvoiceDate DATETIME;
 ALTER TABLE onlineretail MODIFY CustomerID INT;
 ALTER TABLE onlineretail MODIFY UnitPrice DECIMAL(10,2);
+ALTER TABLE onlineretail MODIFY StockCode INT;
 DESC onlineretail;
+
+
+-- ------------------------------ Modify Empty, Null and 0 cells --------------------------------------------------
+
+-- ------ Modify Empty, Null and 0 cells in StockCode column
+
+SELECT InvoiceNo, StockCode
+FROM onlineretail
+WHERE StockCode IS NULL OR StockCode = ' ' OR StockCode = 0;
+
+UPDATE onlineretail
+SET StockCode = 001
+WHERE StockCode IS NULL OR StockCode = ' ' OR StockCode = 0;
+
+SELECT InvoiceNo, StockCode
+FROM onlineretail
+WHERE StockCode = 001;
+
+-- ----- Modify Empty, Null and 0 cells in Quantity column
+SELECT InvoiceNo, StockCode, Quantity
+FROM onlineretail
+WHERE Quantity IS NULL OR Quantity = '' OR Quantity <= 0;
+
+SELECT COUNT(Description)
+FROM onlineretail
+WHERE Quantity IS NULL OR Quantity = '' OR Quantity <= 0;
+
+UPDATE onlineretail
+SET Quantity = ABS(Quantity)
+WHERE Quantity < 0;
+
 
